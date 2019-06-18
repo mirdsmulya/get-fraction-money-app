4bbf8f1f5758525e5c96bbe1dc2bd75c69f3e22b
mirdsm <by_ms19@yahoo.com>
2019-06-18 23:56:23 +0700

    added rupiahValidation and rupiahFraction on onCalculate function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-
-import 'babel-polyfill';
+//import 'babel-polyfill';
 import React, {propTypes} from 'react';
 import { render } from 'react-dom';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
+import FractionResult from './components/FractionResult';
+import RupiahValidation from './lib/RupiahValidation';
+import RupiahFraction from './lib/RupiahFraction';
 
 
 class App extends React.Component {
@@ -12,17 +14,19 @@ class App extends React.Component {
 		super(props,context);
 		this.state = {
 			rupiahNumber: " ",
-			result: " "
+			result: []
 		};
 		this.onCalculate = this.onCalculate.bind(this);
 		this.updateInput = this.updateInput.bind(this);
 	}
 
-
 	onCalculate(event) {
 		debugger;
 		let data = Object.assign(this.state.rupiahNumber);
-		this.setState({result: data});
+		let rupiahValid = RupiahValidation.validateInput(data);
+		let result = RupiahFraction.fractionRupiah(rupiahValid);
+		this.setState({result: result});
+
 	}
 
 	updateInput(event) {
@@ -47,14 +51,13 @@ class App extends React.Component {
 						onChange={this.updateInput}
 						value={this.state.rupiahNumber}/>
 				</form>
-				<button
-					className="btn center-btn"
-					onClick={this.onCalculate}>Calculate</button>
-				<p className="center-text">Result:</p>
-				{this.state.rupiahNumber}
-				<div>
-				{this.state.result}
-			</div>
+				<button className="btn center-btn" onClick={this.onCalculate}>
+					Calculate
+				</button>
+				<p className="center-text">
+					Result:
+				</p>
+				{this.state.result.map(data => <FractionResult data={data} />)}
 			</div>
 
 			);
