6570951fd4069664d5ea5227c11e7c91dd69fdf2
mirdsm <by_ms19@yahoo.com>
2019-06-21 16:22:55 +0700

    refactor and cleaning the code

--- src/lib/RupiahValidation.js
+++ src/lib/RupiahValidation.js
@@ -2,22 +2,8 @@
 
 class RupiahValidation {
 
-  static validateInput(input) {
-
-    let splitted = input.split('');
-    let withoutRp = this.cleanRpString(splitted);
-    let missingValueChecked = this.detectMissingValue(withoutRp);
-    let zeroOnFrontChecked = this.cleanZeroOnFront(missingValueChecked);
-    let withoutDotRp = this.cleanDotString(zeroOnFrontChecked);
-    let spaceChecked = this.detectSpaceString(withoutDotRp);
-    let withoutDotRpComa = this.cleanComaString(spaceChecked);
-    let validationResult = this.parseStringToInteger(withoutDotRpComa);
-    debugger;
-    return validationResult;
-  }
-
   static cleanRpString(splittedInput) {
-    let detectRpString = this.detectChar(splittedInput, "R", "p");
+    let detectRpString = this.detectRpChar(splittedInput, "r", "p");
     if (detectRpString) {
       splittedInput.splice(0,2);
       if (splittedInput[0] == " ") {
@@ -27,9 +13,18 @@ class RupiahValidation {
     return splittedInput;
   }
 
+  static detectRpChar(string, detect1, detect2) {
+    let string1 = string[0].toLowerCase();
+    let string2 = string[1].toLowerCase();
+    if (string1 == detect1 && string2 == detect2) {
+      return true;
+    }
+    return false;
+  }
+
   static detectMissingValue(splittedInput) {
     if (splittedInput.length == 0) {
-      splittedInput.push(',');
+      splittedInput.push('error');
     }
     return splittedInput;
   }
@@ -40,28 +35,24 @@ class RupiahValidation {
       if (input[0] == "0") {
         input.shift();
       }
-      else {i = splittedInput.length; }
+      else {i = splittedInput.length; } //end loop
     }
     return input;
   }
 
   static cleanDotString(splittedInput) {
-    let a = 1, b = 2, c = 3;
+    let a = 1, b = 2, c = 3; // for input 1.000
     for (let i = 0; i < 5; i++ ) {
       let indexOfDot = splittedInput.indexOf('.');
-      // define dot ('.') position
       if (indexOfDot == a || indexOfDot == b|| indexOfDot == c ) {
         splittedInput.splice(indexOfDot, 1 );
-        if (i == 0) {a = 4; b = 5; c = 6;} // variable for input 100.000 / 100.000.000 / 100.000 /
-        if (i == 1) {a = 7; b = 8; c = 9;} // 100.000.000
-        if (i == 2) {a = 10; b = 11; c = 12;} // 100.000.000.000
-        if (i == 3) {a = 13; b = 14; c = 15;}
+        if (i == 0) {a = 4 ; b = 5 ; c =  6;} // for input 1.000.000
+        if (i == 1) {a = 7 ; b = 8 ; c =  9;} // for input 1.000.000.000
+        if (i == 2) {a = 10; b = 11; c = 12;} // for input 1.000.000.000.000
+        if (i == 3) {a = 13; b = 14; c = 15;} // for input 1.000.000.000.000.000
       }
-      else if (indexOfDot == -1 ) { i = 5; }
-      else {
-        splittedInput.push("error");
-        i = 5;
-      }
+      else if (indexOfDot == -1 ) { i = 5 ; } // i = 5 -> end loop
+      else { splittedInput.push("error"); i = 5;}
     }
     return splittedInput;
   }
@@ -78,35 +69,14 @@ class RupiahValidation {
   static cleanComaString(splittedInput) {
     let list = splittedInput;
     let indexOfComa = list.indexOf(',');
-    if (indexOfComa == list.length - 3 && list.length != 2) {
+    if (indexOfComa == list.length - 3 && list.length != 2) { //ex: Rp17.400,00
       this.popLoop(list,3);
     }
     else if (indexOfComa == -1 ) {
-      //pass
+      //pass, no coma (',') detected
     }
-    else { list = undefined; }
+    else { list = false; } // coma not in correct position
     return list;
-  }
-
-  static parseStringToInteger(inputList) {
-    let result = 0;
-    if (inputList != undefined) {
-      let joinChar = inputList.join("");
-      let to_Integer = parseInt(joinChar);
-      if (to_Integer.toString() != joinChar) {
-        result = false;
-      }
-      else { result = to_Integer; }
-    }
-    else { result = false; }
-    return result;
-  }
-
-  static detectChar(string, detect1, detect2) {
-    if (string[0] == detect1 && string[1] == detect2) {
-      return true;
-    }
-    else {return false;}
   }
 
   static popLoop(input, range) {
@@ -114,6 +84,33 @@ class RupiahValidation {
       input.pop();
     }
     return input;
+  }
+
+  static parseStringToInteger(inputList) {
+    let result = 0;
+    if (inputList) {
+      let joinedChar = inputList.join("");
+      let integerResult = parseInt(joinedChar);
+      let stringResult = integerResult.toString();
+      if (stringResult != joinedChar) {
+        result = false;
+      }
+      else { result = integerResult; }
+    }
+    else { result = false; }
+    return result;
+  }
+
+  static validateInput(input) {
+    let splitted = input.split('');
+    let withoutRp = this.cleanRpString(splitted);
+    let missingValueChecked = this.detectMissingValue(withoutRp);
+    let zeroOnFrontChecked = this.cleanZeroOnFront(missingValueChecked);
+    let withoutDotRp = this.cleanDotString(zeroOnFrontChecked);
+    let spaceChecked = this.detectSpaceString(withoutDotRp);
+    let withoutDotRpComa = this.cleanComaString(spaceChecked);
+    let validationResult = this.parseStringToInteger(withoutDotRpComa);
+    return validationResult;
   }
 
 }
