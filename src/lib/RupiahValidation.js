dcc7962a9a813f68c9f2cac370528b3079af26b7
mirdsm <by_ms19@yahoo.com>
2019-06-22 20:15:29 +0700

    add dot position verification and add if (true) method on each function

--- src/lib/RupiahValidation.js
+++ src/lib/RupiahValidation.js
@@ -3,93 +3,137 @@
 class RupiahValidation {
 
   static cleanRpString(splittedInput) {
-    let detectRpString = this.detectRpChar(splittedInput, "r", "p");
+    let detectRpString = this.detectChar(splittedInput, "r", "p");
     if (detectRpString) {
       splittedInput.splice(0,2);
-      if (splittedInput[0] == " ") {
-        splittedInput.shift();
-      }
+      if (splittedInput[0] == " ") { splittedInput.shift(); }
     }
     return splittedInput;
   }
 
-  static detectRpChar(string, detect1, detect2) {
-    if (string.length > 1) {
-      let string1 = string[0].toLowerCase();
-      let string2 = string[1].toLowerCase();
-      if (string1 == detect1 && string2 == detect2) {
-        return true;
-      }
+  static detectChar(string, detect1, detect2) {
+    if (string[0].toLowerCase() == detect1 && string[1].toLowerCase() == detect2) {
+      return true;
     }
-    return false;
+    else {return false; }
   }
 
   static detectMissingValue(splittedInput) {
-    if (splittedInput.length == 0) {
-      splittedInput.push('error');
-    }
+    if (splittedInput.length == 0) { return false; }
     return splittedInput;
   }
 
   static cleanZeroOnFront(splittedInput) {
-    let lengthInput = splittedInput.length;
-    for (let i = 0; i < lengthInput; i++) {
-      if (splittedInput[0] == "0") {
-        splittedInput.shift();
+    if (splittedInput) {
+      const lengthInput = splittedInput.length;
+      for (let i = 0; i < lengthInput; i++) {
+        if (splittedInput[0] == "0") { splittedInput.shift(); }
+        else {i = lengthInput; } //end loop
       }
-      else {i = lengthInput; } //end loop
-    }
-    return splittedInput;
-  }
-
-  static cleanDotString(splittedInput) {
-    let a = 1, b = 2, c = 3; // for input 1.000 - 999.999
-    for (let i = 0; i < 5; i++ ) {
-      let indexOfDot = splittedInput.indexOf('.');
-      if (indexOfDot == a || indexOfDot == b|| indexOfDot == c ) {
-        splittedInput.splice(indexOfDot, 1 );
-        if (i == 0) {a = 4 ; b = 5 ; c =  6;} // for input 1.000.000 - 999.999.999
-        if (i == 1) {a = 7 ; b = 8 ; c =  9;} // for input 1.000.000.000 - 999G
-        if (i == 2) {a = 10; b = 11; c = 12;} // for input 1.000.000.000.000 - 999T
-        if (i == 3) {a = 13; b = 14; c = 15;} // for input 1.000.000.000.000.000 - etc
-      }
-      else if (indexOfDot == -1 ) { i = 5 ; } // i = 5 -> end loop
-      else { splittedInput.push("error"); i = 5;}
     }
     return splittedInput;
   }
 
   static detectSpaceString(splittedInput) {
-    let indexOfSpaceString = splittedInput.indexOf(' ');
-    let inputLength = splittedInput.length;
-    if (indexOfSpaceString > 0) {
-      splittedInput.push('error');
+    if (splittedInput) {
+      let indexOfSpaceString = splittedInput.indexOf(' ');
+      if (indexOfSpaceString > 0) { return false; }
+      return splittedInput;
     }
-    return splittedInput;
   }
 
   static cleanComaString(splittedInput) {
     let list = splittedInput;
-    let indexOfComa = list.indexOf(',');
-    if (indexOfComa == list.length - 3 && list.length != 2) { //ex: Rp17.400,00
-      this.popLoop(list,3);
+    if (splittedInput) {
+      let indexOfComa = list.indexOf(',');
+      if (indexOfComa == list.length - 3 && list.length != 2) {
+        this.popLoop(list,3); //Rp17.400,00 -> Rp17.400
+      }
+      else if (indexOfComa == -1 ) {
+        //pass, no coma (',') detected
+      }
+      else { list = false; } // coma not in correct position
+      return list;
     }
-    else if (indexOfComa == -1 ) {
-      //pass, no coma (',') detected
-    }
-    else { list = false; } // coma not in correct position
-    return list;
+
   }
 
   static popLoop(input, range) {
-    for (let i = 0; i < range; i++) {
-      input.pop();
-    }
+    for (let i = 0; i < range; i++) { input.pop(); }
     return input;
   }
 
+  static checkDotString(input) {
+    const splittedInput = input ;
+    if (input) {
+      let indexOfDot = splittedInput.indexOf('.');
+      let freeDotNumbersAndListDotIndex = this.cleanDotStr(input);
+      let freeDotNumbers = freeDotNumbersAndListDotIndex[0].input;
+      let listIndexOfDots = freeDotNumbersAndListDotIndex[0].index;
+      let lengthFreeDotNumber = freeDotNumbers.length;
+      let listCorrectDotIndex = this.listClassificationOfCorrectDotIndex(listIndexOfDots[0], lengthFreeDotNumber);
+      let isBothArrayIsTheSame = this.compareListObject(listIndexOfDots,listCorrectDotIndex);
+      if (isBothArrayIsTheSame || indexOfDot == -1) {
+        return splittedInput;
+      }
+      return false;
+    }
+    return splittedInput;
+  }
+
+  static cleanDotStr(inputs) {
+    let length = inputs.length;
+    let temp = [];
+    let mainTemp = [];
+    for (let i = 0; i < 6; i++) {
+      let indexOfDot = inputs.indexOf('.');
+      if (indexOfDot > 0) {
+        temp.push(indexOfDot);
+        inputs.splice(indexOfDot,1);
+      }
+      else if (indexOfDot == -1 && length > 3 ) {
+        let lengthTemp = temp.length;
+        let length2 = temp[lengthTemp-1] + 3;
+        temp.push(length2);
+        i = 6;
+      }
+      else {
+        temp.push(1);
+        i = 6;
+      }
+    }
+    mainTemp.push({index: temp, input: inputs});
+    return mainTemp;
+  }
+
+  static listClassificationOfCorrectDotIndex(firstDotIndex, length) {
+    let temp = [];
+    if (length > 3) {
+      let iterateNumb = parseInt( (length - 1) / 3);
+      let indexOfDot = firstDotIndex;
+      for (let i = 0; i < iterateNumb; i++ ) {
+        temp.push(indexOfDot);
+        indexOfDot += 3;
+      }
+      temp.push(length);
+      return temp;
+    }
+    temp.push(1);
+    return temp;
+  }
+
+  static compareListObject(inputA, inputB) {
+    if (inputA.length == inputB.length) {
+      for (let i = 0; i < inputB.length; i++) {
+        if (inputA[i] != inputB[i]) { return false; }
+      }
+      return true;
+    }
+    return false;
+  }
+
   static parseStringToInteger(inputList) {
-    let result = 0;
+    let result;
     if (inputList) {
       let joinedChar = inputList.join("");
       let integerResult = parseInt(joinedChar);
@@ -104,15 +148,18 @@ class RupiahValidation {
   }
 
   static validateInput(input) {
-    let splitted = input.split('');
-    let withoutRp = this.cleanRpString(splitted);
-    let missingValueChecked = this.detectMissingValue(withoutRp);
-    let zeroOnFrontChecked = this.cleanZeroOnFront(missingValueChecked);
-    let withoutDotRp = this.cleanDotString(zeroOnFrontChecked);
-    let spaceChecked = this.detectSpaceString(withoutDotRp);
-    let withoutDotRpComa = this.cleanComaString(spaceChecked);
-    let validationResult = this.parseStringToInteger(withoutDotRpComa);
-    return validationResult;
+      let splitted = input.split('');
+      let withoutRp = this.cleanRpString(splitted);
+      let missingValueChecked = this.detectMissingValue(withoutRp);
+      let zeroOnFrontChecked = this.cleanZeroOnFront(missingValueChecked);
+      let spaceChecked = this.detectSpaceString(zeroOnFrontChecked);
+      let withoutRpComa = this.cleanComaString(spaceChecked);
+      let withoutRpComaDot = this.checkDotString(withoutRpComa);
+      let validationResult = this.parseStringToInteger(withoutRpComaDot);
+      return validationResult;
+
+
+
   }
 
 }
